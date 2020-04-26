b43c9edca183e24720f6f462ee74c49c5da649f0
Ryan Chomistek <35873399+RyanChomistek@users.noreply.github.com>
2020-04-25 17:18:54 -0700

    adding new device client code

--- Client/main_client.py
+++ Client/main_client.py
@@ -1,13 +1,30 @@
 #!/usr/bin/env python3
-
+import json
 import socket
 
-HOST = '127.0.0.1'  # The server's hostname or IP address
+HOST = '52.229.38.99'  # The server's hostname or IP address
 PORT = 8888        # The port used by the server
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    for i in range(1,10):
-        s.sendall(b'Hello, world')
+deviceConnectionPayload = "start consumer connection"
+
+#print(str(bytes(deviceConnectionPayload, 'utf-8')))
+
+def setupSocket():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        print("start connect")
+        s.connect((HOST, PORT))
+        
+        # Send initial handshake
+        s.sendall(bytes(deviceConnectionPayload, 'utf-8'))
         data = s.recv(1024)
-        print('Received', repr(data))
+        dataStr = data.decode('utf-8')
+
+        print("hand shake response " + dataStr)
+        if(dataStr != "success"):
+            return None
+
+        while True:
+            data = s.recv(1024)
+            print('Received', repr(data))
+
+setupSocket()
